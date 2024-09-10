329671f044dda284b845fef00feb2b44f07b6db1
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-09-10 05:57:43 -0700

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function isSameType(value1, value2) {
     return typeof value1 === typeof value2;
 }
 
-console.log(isSameType("hello", "world"));  
-
-console.log(isSameType(NaN, NaN)); 
+console.log(isSameType(1, 3));        
+console.log(isSameType("hey", "hello"));  
+console.log(isSameType(NaN, NaN));    
+console.log(isSameType("3", 3));      
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
